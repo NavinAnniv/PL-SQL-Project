1678caa94fa9668fcd024ef380c1da7361b1bbe9
Navin S <36127385+NavinAnniv@users.noreply.github.com>
2021-09-04 11:00:39 +0530

    Update Log_Trigger.sql

--- Log_Trigger.sql
+++ Log_Trigger.sql
@@ -1,4 +1,4 @@
--- Created a trigger to capture the update,inserted or deleted records and store it in dept_log table
+-- Created a trigger to capture the update,inserted or deleted records in Department table and store it in dept_log table
 
 CREATE OR REPLACE  TRIGGER dept_log AFTER
     INSERT OR UPDATE OR DELETE ON department
@@ -31,5 +31,42 @@ BEGIN
         :new.emp_id,
         :new.dept_name,
         :new.dept_id
+    );
+END;
+
+
+-- Created a trigger to capture the update,inserted or deleted records in employee table and store it in emp_log table
+
+CREATE OR REPLACE TRIGGER emp_log AFTER
+    INSERT OR UPDATE OR DELETE ON employee
+    FOR EACH ROW
+DECLARE
+    l_transac   VARCHAR2(10);
+    table_name  VARCHAR2(20);
+BEGIN
+    CASE
+        WHEN inserting THEN
+            l_transac := 'insert';
+        WHEN updating THEN
+            l_transac := 'UPDATE';
+        WHEN deleting THEN
+            l_transac := 'DELETE';
+    END CASE;
+
+    table_name := 'employee';
+    INSERT INTO log_data(
+        table_name,
+        transaction_name,
+        transaction_date,
+        emp_id,
+        emp_name,
+        dept_name
+    ) VALUES (
+        table_name,
+        l_transac,
+        to_char(sysdate, 'MM-DD-YYYY HH24:MI:SS'),
+        :new.emp_id,
+        :new.emp_name,
+        :new.dept_name
     );
 END;
